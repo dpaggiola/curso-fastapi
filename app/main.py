7522a7101ced9f015e06bf3a5f8a732c5c7465f5
Daniel Paggiola <dpaggiola@gmail.com>
2026-05-18 16:07:53 -0300

    Clase 19

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import time
+
+from fastapi import FastAPI, Request
 from models import Invoice
 from db import create_all_tables
 from .routers import customers, transactions, plans
@@ -7,6 +9,14 @@ app = FastAPI(lifespan=create_all_tables)
 app.include_router(customers.router)
 app.include_router(transactions.router)
 app.include_router(plans.router)
+
+@app.middleware("http")
+async def log_request_time(request: Request, call_next):
+  start_time = time.time()
+  response = await call_next(request)
+  process_time = time.time() - start_time
+  print(f"Request: {request.url} completed in: {process_time:.4f} seconds")
+  return response
 
 @app.get("/")
 async def root():
